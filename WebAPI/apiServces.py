05a7d0dc5c9b786ddf11b38c6f87835fd724b989
German <german.sviridov@polito.it>
2019-05-24 17:22:35 +0200

    bug fixes

--- WebAPI/apiServces.py
+++ WebAPI/apiServces.py
@@ -224,14 +224,13 @@ class Log(Resource):
     @jwt_required()
     def post(self, group_id):
         data = request.get_json()
-        print data['timestamp']
         # Check content of data
         if 'event_id' not in data or 'timestamp' not in data or 'device_mac' not in data:
             print('Event is missing some fields.')
             exit()
 
         try:
-            datetime.strptime(data['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+            datetime.datetime.strptime(data['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
         except:
             print("Wrong datetime format in event.")
             exit()
